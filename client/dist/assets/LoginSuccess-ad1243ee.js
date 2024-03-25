a39434365b08d99c9cf9139f31c23909372f3cd1
Farida Hasanova <finance.labs1@gmail.com>
2024-03-25 17:19:38 +0400

    update

---
+++ client/dist/assets/LoginSuccess-ad1243ee.js
@@ -0,0 +1,1 @@
+import{i as e,g as o,r as i,j as t,T as r}from"./index-55c54f2d.js";const n=()=>{const a=e(),s=o();return i.useEffect(()=>{localStorage.setItem("firstLogin","true"),s({type:"IS_LOGGED",payload:!0}),s({type:"SUCCESS",payload:"Login Success"}),a("/")},[]),t("div",{children:t(r,{})})};export{n as default};
